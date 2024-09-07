22274d94fccf153ce96053d1ac5f6e9ab939faf9
Mosaddik <mosaddek.b1307@gmail.com>
2024-09-07 06:20:52 +0600

    Vanilla JavaScript Sticky Nav

---
+++ script.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('#main');
+let topOfNav = nav.offsetTop;
+
+function fixNav() {
+    if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+    }
+}
+
+window.addEventListener('scroll', fixNav);
+window.addEventListener('resize', () => {
+    topOfNav = nav.offsetTop;
+});
